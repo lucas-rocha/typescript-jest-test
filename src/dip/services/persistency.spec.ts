6447a0c2d50b55af60f793300e03543592019e80
Lucas Rocha <lucasrocha.dv@gmail.com>
2021-06-28 20:35:26 -0300

    Configure jest

---
+++ src/dip/services/persistency.spec.ts
@@ -0,0 +1,13 @@
+describe('Testando alguma coisa', () => {
+  it('should return one', () => {
+    const number = 1;
+    expect(number).toBe(1);
+  });
+});
+
+describe('Testando outra coisa', () => {
+  test('should return Lucas', () => {
+    const name = 'Lucas';
+    expect(name).toBe('Lucas');
+  });
+});
